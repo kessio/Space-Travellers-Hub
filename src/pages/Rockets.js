eee40f03e00ad37b4e8bc9d8fc5f240a187174d4
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-11 12:38:14 -0700

    add navigation link to Rockets page

---
+++ src/pages/Rockets.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RocketsList from '../components/RocketsList';
+
+const Rockets = () => {
+  return (
+    <div>
+      <RocketsList />
+    </div>
+  )
+}
+
+export default Rockets
