924f19749148cefc28c6c13045ac39f8e4520464
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-11 12:37:51 -0700

    add navigation link to Missions page

---
+++ src/pages/Missions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MissionsList from '../components/MissionsList';
+
+const Missions = () => {
+  return (
+    <div>
+      <MissionsList />
+    </div>
+  )
+}
+
+export default Missions
