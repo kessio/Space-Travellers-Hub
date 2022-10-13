e9ff052d02d17c75255516779439b5b34030a53c
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-13 10:28:35 -0700

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DisplayRockets from './components/DisplayRockets';
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import DisplayRockets from './components/DisplayRockets';
 import NavBar from './components/NavBar';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
-import Profile from './pages/Profile';
+import MyProfile from './components/MyProfile';
 import store from './redux/configureStore';
 import { fetchMisions } from './redux/missions/missionsSlice';
 
@@ -18,14 +18,13 @@ const App = () => {
     <div>
       <NavBar />
       <Routes>
-        <Route exact path="/"  element={<Rockets />} />
+        <Route exact path="/" element={<Rockets />} />
         <Route path="/rockets" element={<Rockets />} />
-        <Route path="/missions" element= {<Missions />} />
-        <Route path="/my-profile" element={<Profile />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/my-profile" element={<MyProfile />} />
       </Routes>
     </div>
   );
 };
 
 export default App;
-
--- src/components/missions/MissionTable.js
+++ src/components/missions/MissionTable.js
@@ -5,32 +5,32 @@ import DisplayMissions from './DisplayMissions';
 
 const MissionTable = () => {
   const missions = useSelector((state) => state.missions.data);
-  console.log(missions)
 
   return (
     <div>
-      <table className="missions-table" >
+      <table className="missions-table">
         <thead>
           <tr className="col-header">
-          <th className="col-header">Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th></th>
+            <th className="col-header">Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {missions.map((mission) => (
-              <DisplayMissions
-                key={uuidv4()}
-                id={mission.mission_id}
-                name={mission.mission_name}
-                description={mission.description}
-                isReserved={mission.reserved} />
+            <DisplayMissions
+              key={uuidv4()}
+              id={mission.mission_id}
+              name={mission.mission_name}
+              description={mission.description}
+              isReserved={mission.reserved}
+            />
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default MissionTable
+export default MissionTable;
