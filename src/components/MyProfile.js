14e936c3e10c5adc86dfde8b877cdc0db6c7d10d
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-13 16:24:18 -0700

    add stlyes to My Profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -5,18 +5,19 @@ import ListReservedMissions from './missions/ListReservedMissions';
 
 function MyProfile() {
   return (
-    <div className="profile">
-      <div>
-        <h3>My Missions</h3>
+    <div className="profilesCont">
+      <div className="missionsCont">
+        <h3 className="heading">My Missions</h3>
         <ListReservedMissions />
       </div>
-      <div>
-        <h3>My Rockets</h3>
-        <Card style={{ width: '18rem' }}>
+      <div className="rocketsCont">
+        <h3 className="heading">My Rockets</h3>
+        <Card style={{ width: '40rem' }}>
           <ListGroup variant="flush">
             <ListGroup.Item>Cras justo odio</ListGroup.Item>
             <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
             <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
           </ListGroup>
         </Card>
       </div>
