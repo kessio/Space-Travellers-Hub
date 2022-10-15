8763cd9552996fb53da1c6f979e980d8ffe02aab
kessio <64524758+kessio@users.noreply.github.com>
2022-10-15 12:41:40 +0300

    snapsot test for display rocket compoent

--- src/__tests_/Rocket.test.js
+++ src/__tests_/Rocket.test.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import TestRender from 'react-test-renderer';
 import store from '../redux/configureStore';
+import DisplayRocket from '../components/rockets/DisplayRockets';
 import RocketCard from '../components/rockets/RocketCard';
 
 describe('snapshot test for RocketCard Component', () => {
     it('Matches the snapshot', () => {
       const tree = TestRender.create(<RocketCard />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('snapshot test for DisplayRocket Component', () => {
+    it('Matches the snapshot', () => {
+      const tree = TestRender.create(
+        <Provider store={store}>
+        <DisplayRocket />
+      </Provider>,
+      ).toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
