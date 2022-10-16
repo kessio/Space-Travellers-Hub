449f338273fd9bf8478deb4c7939d6f13b63ffc3
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-15 18:51:30 -0700

    added redux-logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,19 @@
-import { combineReducers } from 'redux';
+import { combineReducers, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsSlice from './rockets/rockets';
 import missionsSlice from './missions/missionsSlice';
+import logger from 'redux-logger';
 
 const allReducers = combineReducers({
   rockets: rocketsSlice,
   missions: missionsSlice.reducer,
 });
 
+const middlewares = [logger];
 const store = configureStore({
   reducer: allReducers,
-});
+},
+applyMiddleware(logger),
+);
 
 export default store;
